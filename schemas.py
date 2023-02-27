77c72a1a978022df911b0044914cdaa36dda1855
AlexandrBozhok <sasha.bozhok23@gmail.com>
2023-02-28 01:07:54 +0200

    Add 3 new fields to Product schema

--- schemas.py
+++ schemas.py
@@ -1,7 +1,11 @@
-from pydantic import BaseModel
+import datetime
+
+from pydantic import BaseModel, validator
 
 
 class Product(BaseModel):
+    created: datetime.datetime = None
+    updated: datetime.datetime = None
     name: str
     price: int
     url: str
@@ -9,3 +13,12 @@ class Product(BaseModel):
     material: str | None = None
     circulation: int | str | None = None
     year_of_production: int
+    sold_out: bool = False
+
+    @validator('created', pre=True, always=True)
+    def set_created(cls, v):
+        return v or datetime.datetime.now()
+
+    @validator('updated', pre=True, always=True)
+    def set_updated(cls, v):
+        return v or datetime.datetime.now()
